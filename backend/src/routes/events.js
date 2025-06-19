b7b6c0a2f295270987456006a111d869b335fcf5
Alyson Gadelha <alyson_sud@hotmail.com>
2025-06-19 17:02:45 +0100

    feat: add new services and routes to events

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -1,5 +1,10 @@
 import { getPostById } from '../services/posts.js'
-import { trackEvent } from '../services/events.js'
+import {
+  trackEvent,
+  getTotalViews,
+  getDailyViews,
+  getDailyDurations,
+} from '../services/events.js'
 
 export const eventRoutes = (app) => {
   app.post('/api/v1/events', async (req, res) => {
@@ -14,6 +19,48 @@ export const eventRoutes = (app) => {
       return res.json({ session: event.session })
     } catch (error) {
       console.error('error tracking action', error)
+      return res.status(500).end()
+    }
+  })
+
+  app.get('/api/v1/events/totalViews/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+
+      const stats = await getTotalViews(post._id)
+      return res.json(stats)
+    } catch (error) {
+      console.error('error getting stats', error)
+      return res.status(500).end()
+    }
+  })
+
+  app.get('/api/v1/events/dailyViews/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+
+      const stats = await getDailyViews(post._id)
+      return res.json(stats)
+    } catch (error) {
+      console.error('error getting stats', error)
+      return res.status(500).end()
+    }
+  })
+
+  app.get('/api/v1/events/dailyDurations/:postId', async (req, res) => {
+    try {
+      const { postId } = req.params
+      const post = await getPostById(postId)
+      if (post === null) return res.status(400).end()
+
+      const stats = await getDailyDurations(post._id)
+      return res.json(stats)
+    } catch (error) {
+      console.error('error getting stats', error)
       return res.status(500).end()
     }
   })
--- backend/src/services/events.js
+++ backend/src/services/events.js
@@ -11,3 +11,86 @@ export const trackEvent = async ({
 
   return await event.save()
 }
+
+export const getTotalViews = async (postId) => ({
+  views: await Event.countDocuments({
+    post: postId,
+    action: 'startView',
+  }),
+})
+
+export const getDailyViews = async (postId) =>
+  await Event.aggregate([
+    {
+      $match: {
+        post: postId,
+        action: 'startView',
+      },
+    },
+    {
+      $group: {
+        _id: {
+          $dateTrunc: {
+            date: '$date',
+            unit: 'day',
+          },
+        },
+        views: { $count: {} },
+      },
+    },
+    {
+      $sort: { _id: 1 },
+    },
+  ])
+
+export const getDailyDurations = async (postId) =>
+  await Event.aggregate([
+    {
+      $match: {
+        post: postId,
+      },
+    },
+    {
+      $project: {
+        session: '$session',
+        startDate: {
+          $cond: [
+            {
+              $eq: ['action', 'startView'],
+            },
+            '$date',
+            undefined,
+          ],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: '$session',
+        startDate: { $min: '$startDate' },
+        endDate: { $max: '$endDate' },
+      },
+    },
+    {
+      $project: {
+        day: {
+          $dateTrunc: {
+            date: 'startDate',
+            unit: 'day',
+          },
+        },
+        duration: {
+          $subtract: ['$endDate', '$startDate'],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: '$day',
+        averageDuration: { $avg: '$duration' },
+      },
+    },
+    {
+      $sort: { _id: 1 },
+    },
+  ])
