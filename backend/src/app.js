15a68e6e134d1ccf74005d1e5c8b51cc9f5ef66c
Alyson Gadelha <alyson_sud@hotmail.com>
2025-06-21 16:46:49 +0100

    feat: implement graphql API in the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@as-integrations/express5'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
 import { eventRoutes } from './routes/events.js'
+import { typeDefs, resolvers } from './graphql/index.js'
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+apolloServer
+  .start()
+  .then(() => app.use('/graphql', expressMiddleware(apolloServer)))
+
 postsRoutes(app)
 userRoutes(app)
 eventRoutes(app)
